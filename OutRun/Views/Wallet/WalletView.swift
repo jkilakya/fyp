4cefee437cfc4133bfd3c2d0fddaef0235ce6d3b
Aayush Batwara <aayush@Aayushs-MacBook-Pro.local>
2023-03-29 00:49:06 +0800

    edited project settings

--- OutRun/Views/Wallet/WalletView.swift
+++ OutRun/Views/Wallet/WalletView.swift
@@ -1,32 +1,151 @@
 //
-//  [FILENAME]
+//  WalletView.swift
+//  trialApp
 //
-//  OutRun
-//  Copyright (C) 2020 Tim Fraedrich <timfraedrich@icloud.com>
-//
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//  Created by Ilakya JK on 26/3/2023.
 //
 
 import SwiftUI
 
 struct WalletView: View {
+    @available(iOS 13.0, *)
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        
+        VStack(spacing:10.0) {
+            
+            Text("My Wallet")
+                .padding(.all)
+                .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+            
+            Spacer()
+            
+            VStack{
+                
+                
+                
+                Text("Available balance")
+                Text("0 AKM + 1000 USDC")
+                
+
+                
+                HStack {
+                    
+                    if #available(iOS 14.0, *) {
+                        Button {
+                            print("Deposit button was tapped")
+                        } label: {
+                            Label("Deposit", systemImage: "square.and.arrow.down.fill")
+                        }
+                        .padding(.all)
+                        .frame(width: 122.0)
+                        .accessibilityAddTraits([.isButton])
+                        .accessibilityLabel(/*@START_MENU_TOKEN@*/"Label"/*@END_MENU_TOKEN@*/)
+                        .accentColor(/*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/)
+                        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.orange/*@END_MENU_TOKEN@*/)
+                        .font(/*@START_MENU_TOKEN@*/.headline/*@END_MENU_TOKEN@*/)
+                    } else {
+                        // Fallback on earlier versions
+                    }
+                    
+                    
+                    
+                    if #available(iOS 14.0, *) {
+                        Button {
+                            print("Swap button was tapped")
+                        } label: {
+                            Label("Swap", systemImage: "rectangle.2.swap")
+                        }
+                        .padding(.all)
+                        .frame(width: 122.0)
+                        .accessibilityAddTraits([.isButton])
+                        .accessibilityLabel(/*@START_MENU_TOKEN@*/"Label"/*@END_MENU_TOKEN@*/)
+                        .accentColor(/*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/)
+                        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.orange/*@END_MENU_TOKEN@*/)
+                        .font(/*@START_MENU_TOKEN@*/.headline/*@END_MENU_TOKEN@*/)
+                    } else {
+                        // Fallback on earlier versions
+                    }
+                    
+                    
+                    
+                    
+                    if #available(iOS 14.0, *) {
+                        Button {
+                            print("Withdraw button was tapped")
+                        } label: {
+                            if #available(iOS 14.0, *) {
+                                Label("Withdraw", systemImage: "square.and.arrow.up.fill")
+                            } else {
+                                // Fallback on earlier versions
+                            }
+                        }
+                        .padding(.all)
+                        .frame(width: 136.0)
+                        .accessibilityAddTraits([.isButton])
+                        .accessibilityLabel(/*@START_MENU_TOKEN@*/"Label"/*@END_MENU_TOKEN@*/)
+                        .accentColor(/*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/)
+                        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.orange/*@END_MENU_TOKEN@*/)
+                        .font(/*@START_MENU_TOKEN@*/.headline/*@END_MENU_TOKEN@*/)
+                    } else {
+                        // Fallback on earlier versions
+                    }
+                    
+                    
+                }//end of buttons HStack
+                
+            }//end of top VStack
+            
+            Spacer()
+            
+            Text("Previous Rewards")
+            
+            VStack{
+                
+                HStack{
+                    Image(/*@START_MENU_TOKEN@*/"Image Name"/*@END_MENU_TOKEN@*/)
+                    Text("January 2023")
+                    Text("+ 0 AKM")
+                    
+                }
+                
+                Spacer()
+                
+                HStack{
+                    Image("Image Name")
+                    Text("December 2022")
+                    Text("+ 0 AKM")
+                    
+                }
+                
+                Spacer()
+                
+                HStack{
+                    Image(/*@START_MENU_TOKEN@*/"Image Name"/*@END_MENU_TOKEN@*/)
+                    Text("November 2022")
+                    Text("+ 0 AKM")
+                    
+                }
+                
+                Spacer()
+                
+                HStack{
+                    Image(/*@START_MENU_TOKEN@*/"Image Name"/*@END_MENU_TOKEN@*/)
+                    Text("October 2022")
+                    Text("+ 0 AKM")
+                    
+                }
+            }
+            
+          
+            
+        }//end of VStack
+
     }
 }
 
 struct WalletView_Previews: PreviewProvider {
+    @available(iOS 13.0, *)
     static var previews: some View {
         WalletView()
     }
